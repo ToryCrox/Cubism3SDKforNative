f04e90de63b5e7a5f40f9e2456775516cd74b25d
ToryCrox <exceedutow@gmail.com>
2019-06-05 16:47:28 +0800

    修改后缀常量

--- live2d3/src/main/java/com/mimikko/live2d3/Live2d3Define.java
+++ live2d3/src/main/java/com/mimikko/live2d3/Live2d3Define.java
@@ -8,7 +8,7 @@ package com.mimikko.live2d3;
 public class Live2d3Define {
     public static final int VERSION = 3;
 
-    public static final String MODEL_FILE_EXT = ".model.json";
+    public static final String MODEL_FILE_EXT = ".model3.json";
 
     public static final String FILE_MODE_HITAREAS = "HitAreas";
     public static final String FILE_MODE_PHYSICS = "Physics";
